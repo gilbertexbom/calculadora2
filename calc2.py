3b0ba91c6949b4bf86ae49d9873642092467008d
gilbertexbom <gilberto.braga0021@gmail.com>
2023-11-30 13:10:44 -0300

    - Commit do tio.

---
+++ calc2.py
@@ -0,0 +1,15 @@
+# Calculadora simples otimizada
+
+
+def soma(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+# Teste
+# print(f'{2} + {2} = {soma(2, 2)}')
+# print(f'{2} - {2} = {sub(2, 2)}')
+
+
+
---
+++ calculadora.py
@@ -0,0 +1,16 @@
+# Interface da calculadora
+
+from calc2 import soma
+
+# Apresentação
+print('\n\t\t\t -- Calculadora 2 --\n')
+
+# Entrada
+num1 = int(input('Informe o n1: '))
+num2 = int(input('Informe o n2: '))
+
+# Processamento
+total = soma(num1, num2)
+
+# Saída
+print(f'{num1} + {num2} = {total}')
